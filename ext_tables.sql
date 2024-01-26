65ec3681d53d0b228350c86f20612a63ccde6142
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2024-01-26 17:53:01 +0100

    Extend gf-2cols with horizontal and vertical orientation.

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,6 +8,8 @@ CREATE TABLE tt_content (
     gf_sitepackage_gutter_x tinytext,
     gf_sitepackage_gutter_y tinytext,
     gf_sitepackage_type varchar(60) DEFAULT '' NOT NULL,
+    gf_sitepackage_orientation_horizontal varchar(60) DEFAULT '' NOT NULL,
+    gf_sitepackage_orientation_vertical varchar(60) DEFAULT '' NOT NULL,
     space_inner_before_class varchar(60) DEFAULT '' NOT NULL,
     space_inner_after_class varchar(60) DEFAULT '' NOT NULL,
 );
